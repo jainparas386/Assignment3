5ec7d6bed734916cae60c32128711d187e728b48
jainparas386 <jain.paras@iitgn.ac.in>
2023-04-27 04:28:08 +0530

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
 import plotly as px
 import matplotlib.pyplot as plt
 from sklearn import metrics 
@@ -15,13 +16,12 @@ def generate_data(dataset):
     if dataset == 'Dataset 1':
         X, y = make_classification(n_samples=1000, n_features=2,n_redundant=0,n_classes=2, weights=[0.9, 0.1], random_state=1)
     elif dataset == 'Dataset 2':
-        X, y = make_classification(n_samples=1000, n_features=2,n_redundant=0,n_classes=2, weights=[0.8, 0.2], random_state=2)
+        X, y = make_classification(n_samples=1000, n_features=2,n_redundant=0,n_classes=2, weights=[0.1, 0.9], random_state=2)
     elif dataset == 'Dataset 3':
-        X, y = make_classification(n_samples=1000, n_features=2,n_redundant=0,n_classes=2, class_sep=0.2, weights=[0.7, 0.3], random_state=3)
+        X, y = make_classification(n_samples=1000, n_features=2,n_redundant=0,n_classes=2, class_sep=1, weights=[0.5, 0.5], random_state=3)
     elif dataset == 'Dataset 4':
-        X, y = make_classification(n_samples=1000, n_features=2,n_classes=2, n_redundant=0,class_sep=0.2, weights=[0.6, 0.4], random_state=6)
-    elif dataset == 'Dataset 5':
-        X, y = make_classification(n_samples=1000, n_classes=2, n_features=2,n_redundant=0, n_informative=2)
+        X, y = make_classification(n_samples=1000, n_features=2,n_classes=2, n_redundant=0,class_sep=0.1, weights=[0.5, 0.5], random_state=3)
+    
     return X, y
 
 
@@ -38,20 +38,28 @@ def plot_roc_curve(model, X_test, y_test):
     roc_auc = auc(fpr, tpr)
 
     st.write('### ROC Curve')
+    st.write('ROC(Receiver Operating Characteristics curve) is a graph of TPR vs FPR at different classification thresholds')
     st.write('AUC: {:.3f}'.format(roc_auc))
     # st.line_chart(pd.DataFrame({'fpr': fpr, 'tpr': tpr, 'x': np.linspace(0, 1, 10)}).set_index('fpr').style.set_caption('fpr vs tpr'))
     st.line_chart(pd.DataFrame({'fpr': fpr, 'tpr': tpr}).set_index('fpr'))
 
 
+
 def plot_precision_recall_curve(model, X_test, y_test):
     y_pred_proba = model.predict_proba(X_test)[:,1]
     precision, recall, thresholds = precision_recall_curve(y_test, y_pred_proba)
+    # pr_auc = auc(precision, recall)
 
+    
     st.write('### Precision-Recall Curve')
+    st.write('Precision vs Recall at all classification thresholds')
+    # st.write('AUC: {:.3f}'.format(pr_auc))
     st.line_chart(pd.DataFrame(
         {'precision': precision, 'recall': recall}).set_index('recall'))
+    # st.columns
+    # st.write(thresholds)
     # st.line_chart(pd.DataFrame({'precision': precision, 'recall': recall, 'x': np.linspace(0, 1, 10)}).set_index('recall').style.set_caption('recall vs precision'))
-
+    
 
 def plot_data(X, y):
     st.write('### Dataset')
@@ -63,15 +71,23 @@ def run():
     models = {
         'Decision Tree': DecisionTreeClassifier(),
         'Random Forest': RandomForestClassifier(),
-        'Multi-layer Perceptron': MLPClassifier()
+        'Multi-layer Perceptron': MLPClassifier(),
+        'KNN': KNeighborsClassifier()
     }
     
-    datasets = ['Dataset 1', 'Dataset 2', 'Dataset 3', 'Dataset 3', 'Dataset 4', 'Dataset 5']
+    datasets = ['Dataset 1', 'Dataset 2', 'Dataset 3', 'Dataset 3', 'Dataset 4']
     
     st.sidebar.title('Classification Models')
     
     dataset = st.sidebar.selectbox('Select dataset', options=datasets)
-
+    if dataset == 'Dataset 1':
+        st.sidebar.write('### Imbalanced Dataset')
+    if dataset == 'Dataset 2':
+        st.sidebar.write('### Imbalanced Dataset')
+    if dataset == 'Dataset 3':
+        st.sidebar.write('### Easy to classify(class_sep = 1')
+    if dataset == 'Dataset 4':
+        st.sidebar.write('### Hard to classify(class_sep = 0.1)')
     # X, y = generate_data(**datasets[dataset])
     # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     X, y, X_train, X_test, y_train, y_test = get_data(dataset)
@@ -89,21 +105,29 @@ def run():
         clf.set_params(max_depth = max_depth, n_estimators= n_estimators)
 
     if model == 'Multi-layer Perceptron':
-        alpha_L2_regu = st.sidebar.slider('alpha', 0.00, 0.1, 0.01)
+        alpha_L2_regu = st.sidebar.slider('alpha', 0.00, 100.00, 0.5)
         learning_rate_init = st.sidebar.slider('learning_rate_init', 0.00, 0.1, 0.02)
         max_iter = st.sidebar.slider('max_iter', 20, 210, 20)
+        batch_size = st.sidebar.slider('batch_size', 10, 110, 5)
         # don't know how to add multiple layers
         # hidden_layer_sizes = st.sidebar.slider('hidden_layer_sizes', 20, 210, 20)
         if learning_rate_init == 0.00:
             learning_rate_init=0.001 #default
-        # alpha_L2_regu /= 100
+        
         if alpha_L2_regu == 0.00:
             alpha_L2_regu = 0.0001 #default
+        clf.set_params(max_iter=max_iter, alpha= alpha_L2_regu, learning_rate_init=learning_rate_init, batch_size = batch_size)
+
+    if model == 'KNN':
+        n_neighbors = st.sidebar.slider('n_neighbors', min_value = 1, max_value = 50, step = 5)
+        # p is for type of distance used
+        p = st.sidebar.slider('power_parameters', 1, 5, 1)
+        clf.set_params(n_neighbors = n_neighbors, p = p)
         
-        clf.set_params(max_iter=max_iter, alpha= alpha_L2_regu, learning_rate_init=learning_rate_init)
     clf.fit(X_train, y_train)
 
     st.title('Classification Results')
+    st.write('### This app show the ROC curve and Precision-Recall for different dataset using different classification models')
 
     col1, col2 = st.columns([1, 1])
 
@@ -115,7 +139,9 @@ def run():
         ax.set_xlabel('Feature-1')
         ax.set_ylabel('Feature-2')
         fig.legend()
+        # fig.set_size_inches(8,4)
         st.pyplot(fig)
+        
 
         st.write('### Training Dataset')
         # st.pyplot(pd.DataFrame({'x': X[:,0], 'y': X[:,1], 'label': y}).set_index('x'))
@@ -142,8 +168,8 @@ def run():
         # y_pred = clf.predict(X_test)
 
         # st.write('Training Accuracy:{:.3f}'.format(metrics.accuracy_score(y_test, y_pred)))
-        st.write('Training Accuracy:{:.3f}'.format(clf.score(X_train, y_train)))
-        st.write('Accuracy: {:.3f}'.format(clf.score(X_test, y_test)))
+        st.write('Train Accuracy:{:.3f}'.format(clf.score(X_train, y_train)))
+        st.write('Test Accuracy: {:.3f}'.format(clf.score(X_test, y_test)))
 
         plot_roc_curve(clf, X_test, y_test)
         plot_precision_recall_curve(clf, X_test, y_test)
